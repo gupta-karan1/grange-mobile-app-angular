95f686e93884f57dc69463604cf3e19e4f435e35
Karan Gupta <d22124440@mytudublin.ie>
2023-04-23 04:53:42 +0100

    wip

--- src/app/pages/tab4/tab4.page.ts
+++ src/app/pages/tab4/tab4.page.ts
@@ -20,6 +20,8 @@ import { AlertController, LoadingController } from '@ionic/angular';
 })
 export class Tab4Page implements OnInit {
   // profile = null;
+
+  // set profile to null if no profile is found in the database and then set it to the profile variable if a profile is found in the database and initialize the profile variable to null
   profile: { imageUrl: string | null } = { imageUrl: null };
 
   constructor(
@@ -33,6 +35,7 @@ export class Tab4Page implements OnInit {
     try {
       // this.profile = await this.avatarService.getUserProfile();
       // below code sets the profile to null if no profile is found in the database
+      // wait for the profile to be retrieved from the database and then set it to the profile variable or set it to null if no profile is found
       this.profile = (await this.avatarService.getUserProfile()) || {
         imageUrl: null,
       }; // if no profile, set to null
@@ -45,7 +48,7 @@ export class Tab4Page implements OnInit {
 
   // function to open camera and take a photo with slight modifications to the above function
   async addCameraImage() {
-    // async function
+    // async function because it uses await to wait for the camera to open and take a photo and then upload the photo to the database and then reload the profile to get the new image and then display a toast message to the user.
 
     // open camera from web browser and take a photo
     const image = await Camera.getPhoto({
@@ -56,31 +59,38 @@ export class Tab4Page implements OnInit {
     });
 
     if (image) {
-      const loading = await this.loadingController.create();
-      await loading.present();
+      // if image is not null
+      const loading = await this.loadingController.create(); // create a loading controller
+      await loading.present(); // present the loading controller
 
-      const result = await this.avatarService.uploadAvatar(image);
-      await loading.dismiss();
+      const result = await this.avatarService.uploadAvatar(image); // upload the image to the database
+      await loading.dismiss(); // dismiss the loading controller
 
       // reload the profile to get the new image
       this.profile = (await this.avatarService.getUserProfile()) || {
+        // if no profile, set to null
         imageUrl: null,
       };
+      // display a toast message to the user
       if (result) {
+        // if result is true
         const toast = await this.toastController.create({
-          message: 'Profile Pic Updated.',
-          duration: 3000,
+          message: 'Profile Pic Updated.', // display message
+          duration: 3000, // display for 3 seconds
         });
-        await toast.present();
+        await toast.present(); // present the toast message
       }
 
+      // display an alert message to the user
+      // if the upload failed
       if (!result) {
+        // if result is false
         const alert = await this.alertController.create({
-          header: 'Upload failed',
+          header: 'Upload failed', // display message
           message: 'There was a problem uploading your avatar.',
           buttons: ['OK'],
         });
-        await alert.present();
+        await alert.present(); // present the alert message
       }
     }
   }
